7697fd1e1861792f950c787fac6dcf59d53fdd93
i-k-26 <kyo_ko_ku@yahoo.co.jp>
2019-10-04 18:51:40 +0900

    test

---
+++ Laravel_lesson/app/Http/Controllers/SampleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Frameworks;
+
+class SampleController extends Controller
+{
+    public function model($type=null)
+    {
+        $md = new Frameworks();
+        
+        $data = $md->getData($type);
+        
+        return view('sample.model',['data'=>$data]);
+    }
+}
---
+++ Laravel_lesson/resources/views/beginner.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>First Page</title>
+</head>
+
+<body>
+    <p>hello world!!</p>
+
+    <p>{{$data['name']}}さん</p>
+    <p>{{$data['message']}}</p>
+    <p>現在は{{$today}}です</p>
+
+</body>
+
+</html>
